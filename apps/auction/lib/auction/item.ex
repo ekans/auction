6c2423eb7d739e3c623a718a23b01132271efb06
Ekans <ghekans@gmail.com>
2019-12-01 22:46:30 +0100

    p.142 Validating title presence and length

--- apps/auction/lib/auction/item.ex
+++ apps/auction/lib/auction/item.ex
@@ -1,3 +1,15 @@
 defmodule Auction.Item do
-  defstruct [:id, :title, :description, :ends_at]
+  use Ecto.Schema
+
+  schema "items" do
+    field :title, :string
+    field :description, :string
+    field :ends_at, :utc_datetime
+    timestamps()
+  end
+
+  def changeset(item, params \\ %{}) do
+    item
+    |> Ecto.Changeset.cast(params, [:title, :description, :ends_at])
+  end
 end
